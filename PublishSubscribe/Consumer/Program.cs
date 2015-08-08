1226dcd0cd734eb8f919ad269da84587ad64bc7a
Kolpakov Denis <kolpakovdenis21@gmail.com>
2015-08-08 16:46:38 +0300

    Third example

---
+++ PublishSubscribe/Consumer/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumer
+{
+    class Program
+    {
+        private const string Exchange = "logs";
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(Exchange, "fanout");
+                    var queue = channel.QueueDeclare().QueueName;
+                    channel.QueueBind(queue, Exchange, "");
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += Consumer_Received;
+                    channel.BasicConsume(queue, true, consumer);
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            var message = Encoding.UTF8.GetString(e.Body);
+            Console.WriteLine("Message recieved: {0}", message);
+        }
+    }
+}
---
+++ PublishSubscribe/Producer/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Producer
+{
+    class Program
+    {
+        private const string Exchange = "logs";
+
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(Exchange, "fanout", false);
+                    string message;
+                    while (!string.IsNullOrEmpty(message = Console.ReadLine()))
+                    {
+                        channel.BasicPublish(Exchange, "", null, Encoding.UTF8.GetBytes(message));
+                        Console.WriteLine("Message was sent: {0}", message);
+                    }
+                }
+            }
+        }
+    }
+}
