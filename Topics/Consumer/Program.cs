9db93e25cc81be98798cc562dedb497a5be8a6a7
Kolpakov Denis <kolpakovdenis21@gmail.com>
2015-08-08 19:17:20 +0300

    Topics

---
+++ Topics/Consumer/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumer
+{
+    class Program
+    {
+        private const string Exchange = "routing_exchange";
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var chanel = connection.CreateModel())
+                {
+                    chanel.ExchangeDeclare(Exchange, "topic");
+                    Console.WriteLine("Enter comma separated match patterns (*.high,app.#)> ");
+                    var patterns = Console.ReadLine().Split(',');
+                    var queue = chanel.QueueDeclare().QueueName;
+                    foreach (var pattern in patterns)
+                    {
+                        chanel.QueueBind(queue, Exchange, pattern);
+                    }
+
+                    var consumer = new EventingBasicConsumer(chanel);
+                    consumer.Received += Consumer_Received;
+                    chanel.BasicConsume(queue, true, consumer);
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            var message = Encoding.UTF8.GetString(e.Body);
+            Console.WriteLine("Message: '{0}' Key: {1}", message, e.RoutingKey);
+        }
+    }
+}
---
+++ Topics/Producer/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Producer
+{
+    class Program
+    {
+        private const string Exchange = "routing_exchange",
+            AskMessageText = "Enter message > ",
+            AskForPriorityText = "Enter app.priority (Example: myapp.high) > ";
+
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(Exchange, "topic");
+                    string message = PromptForString(AskMessageText);
+                    string priority = PromptForString(AskForPriorityText);
+
+                    while (!string.IsNullOrEmpty(message) && !string.IsNullOrEmpty(priority))
+                    {
+                        channel.BasicPublish(Exchange, priority, null, Encoding.UTF8.GetBytes(message));
+                        Console.WriteLine("Emit message '{0}' with key {1}", message, priority);
+
+                        message = PromptForString(AskMessageText);
+                        priority = PromptForString(AskForPriorityText);
+                    }
+                }
+            }
+        }
+
+        private static string PromptForString(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+    }
+}
